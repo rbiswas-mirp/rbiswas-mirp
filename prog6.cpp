1f8be3293aeada5e13e547489d4a1fc541829e87
rbiswas-mirp <rbiswas-mirp@mit.edu>
2017-06-01 03:52:00 +0530

    day4

---
+++ prog6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, number;
+	int max = -100000, smax = -100001;
+	cout << "Enter numbers" <<endl;
+	cin >> n;
+	for (int i = 0; i< n; i++){
+		cout << "next num" << endl;
+		cin >> number;
+		if (number > smax && number <=max){
+			smax = max;
+		
+		}
+		else if (number > max){
+			smax = max;
+			max = number;
+		}
+	}
+	cout << "Max is:: " << max << "second max: " << smax << endl;
+}	
