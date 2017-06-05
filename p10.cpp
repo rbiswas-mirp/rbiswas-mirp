666ca19075168f84de525803de8bbef91557c5fc
rbiswas-mirp <rbiswas-mirp@mit.edu>
2017-06-05 11:45:45 +0530

    more stuff in p10

--- p10.cpp
+++ p10.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	int x[5] = {2, 3, 4, 5};
+	int x[5] = {1, 2, 3, 4, 5};
 	for(int i = 0; i < 5; i++)
 		cout << x[i] <<" ";
-	
 	char name[50] = "my_name";
-	for (int i = 0; i< 7; i++)
+	for (int i = 0; i < 7; i++)
 	{
 		cout << name[i] << " ";
 	}
 	cout << "enter name";
-	cin >> name;
+	char name1[1000];
+	cin >> name1;
+	for(int i = 0; name1[i]!= '\0'; i++)
+	{
+		cout << name1[i] << " ";
+	}
 return 0;
 }
 	                                                                                                                                         
