cd69a8c55bbb5b0100e9b22f19b4aeac55e26bd9
rbiswas-mirp <rbiswas-mirp@mit.edu>
2017-06-05 11:12:56 +0530

    Add files via upload

---
+++ p10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x[5] = {2, 3, 4, 5};
+	for(int i = 0; i < 5; i++)
+		cout << x[i] <<" ";
+	
+	char name[50] = "my_name";
+	for (int i = 0; i< 7; i++)
+	{
+		cout << name[i] << " ";
+	}
+	cout << "enter name";
+	cin >> name;
+return 0;
+}
+	                                                                                                                                         
